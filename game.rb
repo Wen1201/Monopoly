69a20906e878fa926b056310b125288aef86c3da
Wen <gongwen1201@gmail.com>
2023-01-20 22:18:39 +1100

    do the same thing with one of the dice files

--- game.rb
+++ game.rb
@@ -1,8 +1,16 @@
 require 'json'
+require 'pry'
 
+# first step is to load the board.json file into a Ruby variable so I can use it in the game code
 file = File.read('board.json')
 data_array = JSON.parse(file)
-print data_array[1]
+print data_array
+
+# do the same thing with one of the dice files - and then how I would iterate over those dice roll numbers and use the rolls to move the players around the board
+file = File.read('rolls_1.json')
+dice_rolls = JSON.parse(file)
+print dice_rolls
 
 
-
+require 'pry'
+binding.pry
