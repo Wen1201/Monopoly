5dc5c523ab6078360fe45aaab727fce4b4da6d80
Wen <gongwen1201@gmail.com>
2023-01-22 16:15:05 +1100

    made a move_player function, check whether they have a buy the property at that new position (if it's available), or pay rent (if it is already owned by another player)

--- game.rb
+++ game.rb
@@ -3,8 +3,8 @@ require 'pry'
 
 # First step is to load the board.json file into a Ruby variable so I can use it in the game code.
 file = File.read('board.json')
-data_array = JSON.parse(file)
-puts data_array
+board = JSON.parse(file) 
+puts board
 
 # do the same thing with one of the dice files. 
 file = File.read('rolls_1.json')
@@ -39,15 +39,28 @@ players = [
     }
 ]
 
+def move_player( player, dice_roll, board)
+    player[:position] += dice_roll
+    current_space = board[player[:position] % board.length]
+    puts "The player is on #{current_space[:name]}"
+    
+end
+
 # a loop that prints out both the dice throw from the array, and the index of each throw 
 dice_rolls.each_with_index do |dice_roll, index|
     # use index to create a new index into the players so it can take turns from 0,1,2,3 
     # and then "wrap" back to 0,1,2,3
     player_index = index % players.length
     # add the current dice roll to the position of the player at that inedx
-    players[player_index][:position] += dice_roll
-    puts "#{players[player_index][:name]}: #{players[player_index][:position]}"   
-end
+    # players[player_index][:position] += dice_roll
+    current_player = players[player_index]
+    # puts "#{players[player_index][:name]}: #{players[player_index][:position]}"   
+
+    # start defining functions 
+    move_player(current_player, dice_roll, board)
+               
+end 
+
 
 
 
